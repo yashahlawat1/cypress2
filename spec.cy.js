b9fd6def388ee8ae0f459a6cce0741aba9054b58
yashahlawat1 <160497536+yashahlawat1@users.noreply.github.com>
2024-03-09 19:55:07 +0530

    Add files via upload

--- spec.cy.js
+++ spec.cy.js
@@ -13,21 +13,23 @@ describe('Template Spec', async () => {
     cy.get('[id="password"]').type('ECsh9shv$2XG7cnG');
     cy.get('[data-testid="login"]').click();
 
-    cy.get('[data-spaceweb="stack"] [data-nav-id="draftPosts"]', { timeout: 120000 }).click();
-
-    cy.get('[data-nav-id="draftPosts"]', { timeout: 120000 }).click({ multiple: true });
-    cy.get('[data-testid="post-card-MESSAGE_17613149"]', { timeout: 120000 }).click({ multiple: true });
+    cy.get('[class="sw--d2 sw--ai sw--bg sw--bq sw--br sw--bs sw--bt sw--im sw--ds sw--fr sw--fs sw--d5 sw--d6 sw--d7 sw--d8 sw--h7 sw--h8 sw--h9 sw--ha sw--b8 sw--c3 sw--c4 sw--c5 sw--c6 sw--a13 sw--a14 sw--f0 sw--ca sw--cb sw--a0r sw--cd sw--da sw--f1 sw--ez sw--ht sw--f3 sw--ly sw--f4 sw--f5 sw--f6 sw--f7 sw--a10 sw--a15 sw--a16 sw--bi sw--ir"]', { timeout: 120000 }).click({multiple:true});
+    cy.get('[data-nav-id="allAssets"]',{timeout:120000}).click({multiple:true});
+    cy.get('[alt="Screenshot 2024-01-17 at 11.22.12 AM"]',{timeout:12000}).click({multiple:true});
+    cy.get('[data-errorid="tags"]',{timeout:120000}).click({multiple:true});
 
     const randomText = faker.internet.userName();
 
-    cy.get('[data-testid="tags-value-box"]').click();
+    cy.get('[role="combobox"]').click();
     cy.get('[data-errorid="tags"]').type(randomText, { force: true });
 
     cy.get('[data-baseweb="popover"] li').click({ multiple: true });
 
-    cy.get('[data-testid="tags-value-box"] span').should("contain", randomText);
+    cy.get('[aria-multiselectable="true"] span').should("contain", randomText);
 
-    cy.get('[data-testid="tags-value-box"]').type('{esc}');
+    cy.get('[aria-multiselectable="true"]').type('{esc}');
+    cy.get('[data-errorid="campaignIds"]').click();
+
   });
 });
 
